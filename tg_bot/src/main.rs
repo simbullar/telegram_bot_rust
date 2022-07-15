98a115b96467f508f1a52baf7ef9f821564a261a
simbullar <86300574+simbullar@users.noreply.github.com>
2022-07-15 14:32:23 +0300

    update 1

--- tg_bot/src/main.rs
+++ tg_bot/src/main.rs
@@ -1,6 +1,13 @@
 use std::net::Ipv4Addr;
 use ip_network::Ipv4Network;
 
+let ip_network = Ipv4Network::new(Ipv4Addr::new(192,168,88,21), 24)?;
+assert_eq!(Ipv4Addr::new(192,168,88,21), ip_network.network_address());
+assert_eq!(24, ip_network.netmask());
+assert_eq!(254, ip_network.hosts().len());
+assert_eq!("192.168.88.21/24", ip_network.to_string());
+
+
 fn main()
 {
     match open::that(r"https://api.telegram.org/bot5407477830:AAHRzUQ1_Yt-TQmhgYfqrTfEe3HWgXMNSJM/sendMessage?chat_id=856546075\&parse_mode=HTML&text=hello") {
